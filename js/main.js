74fbfce73695078635d8869d3994116a70285f26
Isabelle Coimbra <isabellecoimbra00@gmail.com>
2024-11-11 13:08:03 -0300

    implementar o carrosel

--- js/main.js
+++ js/main.js
@@ -10,6 +10,27 @@
     });
 
 
+    const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.carousel-button.next');
+    const prevButton = document.querySelector('.carousel-button.prev');
+
+    let currentSlideIndex = 0;
+
+    function updateSlidePosition() {
+        track.style.transform = `translateX(-${currentSlideIndex * 100}%)`;
+    }
+
+    nextButton.addEventListener('click', () => {
+        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+        updateSlidePosition();
+    });
+
+    prevButton.addEventListener('click', () => {
+        currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+        updateSlidePosition();
+    });
+
 
 
 })();
